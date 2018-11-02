f5574d836a3185fc7c0159159e290d494eb2c102
Stephan Preibisch <stephan.preibisch@gmx.de>
2018-11-02 11:53:26 +0100

    added drawing capabilities

--- src/main/java/fit/circular/Circle2D.java
+++ src/main/java/fit/circular/Circle2D.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import fit.AbstractFunction2D;
+import ij.gui.Line;
+import ij.gui.OvalRoi;
+import ij.gui.Overlay;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
 import mpicbg.models.IllDefinedDataPointsException;
 import mpicbg.models.NotEnoughDataPointsException;
 import mpicbg.models.Point;
@@ -14,25 +19,83 @@ public class Circle2D extends AbstractFunction2D< Circle2D >
 
 	final static int minNumPoints = 3;
 
-	double x, y, r; // m*x + n
+	double u, v, r; // ( x − u )^2 + ( y − v )^2= r
 
 	public Circle2D() { this( 0, 0, 0 ); }
 	public Circle2D( final double x, final double y, final double r )
 	{
-		this.x = x;
-		this.y = y;
+		this.u = x;
+		this.v = y;
 		this.r = r;
+	}
+
+	public static Circle2D unitcircle()
+	{
+		return new Circle2D( 0, 0, 1 );
+	}
+
+	public void drawAxes( final Overlay overlay )
+	{
+		overlay.add( new OvalRoi( u - 3, v - 3, 6, 6 ) );
+		overlay.add( new Line( u, v, u + r, v ) );
+		overlay.add( new Line( u, v, u, v + r ) );
+		overlay.add( new Line( u, v, u - r, v ) );
+		overlay.add( new Line( u, v, u, v - r ) );
+	}
+
+	public double getPointXAt( final double t )
+	{
+		return u + r * Math.cos( t );
+	}
+
+	public double getPointYAt( final double t )
+	{
+		return v + r * Math.sin( t );
+	}
+
+	public void drawCenter( final Overlay overlay )
+	{
+		overlay.add( new Line( u, v - 5, u, v + 5 ) );
+		overlay.add( new Line( u - 5, v, u + 5, v ) );
+
+		overlay.add( new Line( u, v - 5, u + 5, v ) );
+		overlay.add( new Line( u + 5, v, u, v + 5 ) );
+		overlay.add( new Line( u, v + 5, u - 5, v ) );
+		overlay.add( new Line( u - 5, v, u, v - 5 ) );
+	}
+
+	public void draw( final Overlay overlay, final double step )
+	{
+		final ArrayList< Double > xPoints = new ArrayList< Double >();
+		final ArrayList< Double > yPoints = new ArrayList< Double >();
+
+		for ( double t = 0; t < 2*Math.PI; t += step )
+		{
+			xPoints.add( getPointXAt( t ) );
+			yPoints.add( getPointYAt( t ) );
+		}
+
+		final float[] xP = new float[ xPoints.size() ];
+		final float[] yP = new float[ yPoints.size() ];
+
+		for ( int i = 0; i < xP.length; ++i )
+		{
+			xP[ i ] = xPoints.get( i ).floatValue();
+			yP[ i ] = yPoints.get( i ).floatValue();
+		}
+
+		overlay.add( new PolygonRoi( xP, yP, Roi.POLYGON ) );
 	}
 
 	/**
 	 * @return - the center of the circle in x
 	 */
-	public double getX() { return x; }
+	public double getU() { return u; }
 
 	/**
 	 * @return - the center of the circle in y
 	 */
-	public double getY() { return y; }
+	public double getV() { return v; }
 
 	/**
 	 * @return - the radius of the circle
@@ -85,19 +148,19 @@ public class Circle2D extends AbstractFunction2D< Circle2D >
 
 		// calculate center & radius
 		final double f = 0.5 / (uu * vv - uv * uv);
-		this.x = (vv * (uuu + uvv) - uv * (uuv + vvv)) * f;
-		this.y = (-uv * (uuu + uvv) + uu * (uuv + vvv)) * f;
-		this.r = Math.sqrt( this.x * this.x + this.y * this.y + (uu + vv) / total);
+		this.u = (vv * (uuu + uvv) - uv * (uuv + vvv)) * f;
+		this.v = (-uv * (uuu + uvv) + uu * (uuv + vvv)) * f;
+		this.r = Math.sqrt( this.u * this.u + this.v * this.v + (uu + vv) / total);
 
-		this.x += x;
-		this.y += y;
+		this.u += x;
+		this.v += y;
 	}
 
 	@Override
 	public double distanceTo( final Point point )
 	{
-		final double x1 = point.getW()[ 0 ] - getX(); 
-		final double y1 = point.getW()[ 1 ] - getY();
+		final double x1 = point.getW()[ 0 ] - getU(); 
+		final double y1 = point.getW()[ 1 ] - getV();
 
 		return Math.abs( Math.sqrt( x1*x1 + y1*y1 ) - getR() );
 	}
@@ -105,8 +168,8 @@ public class Circle2D extends AbstractFunction2D< Circle2D >
 	@Override
 	public void set( final Circle2D m )
 	{
-		this.x = m.getX();
-		this.y = m.getY();
+		this.u = m.getU();
+		this.v = m.getV();
 		this.r = m.getR();
 		this.setCost( m.getCost() );
 	}
@@ -116,8 +179,8 @@ public class Circle2D extends AbstractFunction2D< Circle2D >
 	{
 		Circle2D c = new Circle2D();
 
-		c.x = getX();
-		c.y = getY();
+		c.u = getU();
+		c.v = getV();
 		c.r = getR();
 		c.setCost( getCost() );
 
@@ -125,7 +188,7 @@ public class Circle2D extends AbstractFunction2D< Circle2D >
 	}
 
 	@Override
-	public String toString() { return "(x − " + getX() + " )^2 + (y − " + getY() + " )^2=" + getR(); }
+	public String toString() { return "(x − " + getU() + " )^2 + (y − " + getV() + " )^2=" + getR(); }
 
 	public static void main( String[] args ) throws NotEnoughDataPointsException, IllDefinedDataPointsException
 	{
