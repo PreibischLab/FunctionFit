8840a13eb5b501d2e40f448d4cec7d6fbf0c7b36
Stephan Preibisch <stephan.preibisch@gmx.de>
2019-01-15 15:15:28 -0500

    implemented ellipse detection in all files of a csv

--- src/main/java/embryo/gui/FindAllEmbryos.java
+++ src/main/java/embryo/gui/FindAllEmbryos.java
@@ -21,7 +21,7 @@ import net.imglib2.util.Pair;
 
 public class FindAllEmbryos
 {
-	public static void processEmbryoimage( final LoadedEmbryo e, final File csv )
+	public static ArrayList< LoadedEmbryo > processEmbryoimage( final LoadedEmbryo e, final File csv )
 	{
 		final File file = new File( csv.getParentFile() + "/masks", e.filename + ".tif" );
 
@@ -35,36 +35,58 @@ public class FindAllEmbryos
 		System.out.println( "Found " + mts.size() + " edge pixels." );
 
 		ImagePlus edgeImp = ImageJFunctions.show( edgeImg );
+		edgeImp.setTitle( e.filename );
 
-		final double minArea = 40000; // minimal size in square-pixels of the ellipse
+		final double minArea = 30000; // minimal size in square-pixels of the ellipse
 		final double maxArea = 90000; // maximal size in square-pixels of the ellipse
+		final double minRatio = 0.999; // ratio = large axis / small axis
+		final double maxRatio = 3.0;
 		final double maxError = 20.0; // maximal distance of an edge pixel to the ellipse and still belong to it
 		final int minNuminliers = 300; // minimal amount of edge pixels that belong to the ellipse
 
 		final ShapePointDistanceFactory< Ellipse, ?, ? > factory = new EllipsePointDistanceFactory();//BruteForceShapePointDistanceFactory< Ellipse >();
 
 		final ArrayList< Pair< Ellipse, ArrayList< PointFunctionMatch > > > functions =
-				Util.findAllFunctions( mts, new Ellipse( factory ), maxError, minNuminliers, minArea, maxArea );
+				Util.findAllFunctions( mts, new Ellipse( factory ), maxError, minNuminliers, minArea, maxArea, minRatio, maxRatio );
 
 		final Overlay o = new Overlay();
 
-		for ( final Pair< Ellipse, ArrayList< PointFunctionMatch > > function : functions )
+		final ArrayList< LoadedEmbryo > embryos = new ArrayList< LoadedEmbryo >();
+
+		if ( functions.size() == 0 )
 		{
-			final Ellipse ellipse = function.getA();
+			final LoadedEmbryo newEmbryo = e.clone();
+			newEmbryo.ellipse = null;
 
-			System.out.println( "isEllipse: " + ellipse.isEllipse() );
-			System.out.println( "area: " + ellipse.area() );
-
-			ellipse.drawCenter( o );
-			ellipse.drawAxes( o );
-			ellipse.draw( o, 0.01 );
-
+			embryos.add( newEmbryo );
 		}
+		else
+		{
+			for ( final Pair< Ellipse, ArrayList< PointFunctionMatch > > function : functions )
+			{
+				final Ellipse ellipse = function.getA();
+	
+				System.out.println( "isEllipse: " + ellipse.isEllipse() );
+				System.out.println( "area: " + ellipse.area() );
+	
+				ellipse.drawCenter( o );
+				ellipse.drawAxes( o );
+				ellipse.draw( o, 0.01 );
 
-		IJ.log( "done" );
+				final LoadedEmbryo newEmbryo = e.clone();
+				newEmbryo.ellipse = ellipse;
+
+				embryos.add( newEmbryo );
+
+			}
+		}
 
 		edgeImp.setOverlay( o );
 		edgeImp.updateAndDraw();
+
+		IJ.log( "done" );
+
+		return embryos;
 	}
 
 	public static void main( String[] args )
@@ -74,14 +96,29 @@ public class FindAllEmbryos
 		final File csvFile = new File( "/Users/spreibi/Documents/BIMSB/Projects/Dosage Compensation/stephan_ellipsoid/stephan_embryo_table3.csv");
 
 		final ArrayList< LoadedEmbryo > embryos = LoadedEmbryo.loadCSV( csvFile );
+		final ArrayList< LoadedEmbryo > annotatedembryos = new ArrayList< LoadedEmbryo >();
 
-		//for ( final LoadedEmbryo e : embryos )
+		for ( final LoadedEmbryo e : embryos )
 		{
-			processEmbryoimage( embryos.get( 0 ), csvFile );
-			
+			annotatedembryos.addAll( processEmbryoimage( e, csvFile ) );
 		}
 
-		//saveCSV( embryos, file );
+		LoadedEmbryo.saveCSV( annotatedembryos, new File( "/Users/spreibi/Documents/BIMSB/Projects/Dosage Compensation/stephan_ellipsoid/stephan_embryo_table3_test.csv") );
+
+		/*
+		final ArrayList< LoadedEmbryo > embryosLoaded = LoadedEmbryo.loadCSV( new File( "/Users/spreibi/Documents/BIMSB/Projects/Dosage Compensation/stephan_ellipsoid/stephan_embryo_table3_test.csv") );
+	
+		ImagePlus imp = IJ.createImage( "test", 1000, 1000, 1, 32 );
+		final Overlay o = new Overlay();
+
+		embryosLoaded.get( 0 ).ellipse.drawCenter( o );
+		embryosLoaded.get( 0 ).ellipse.drawAxes( o );
+		embryosLoaded.get( 0 ).ellipse.draw( o, 0.01 );
+
+		imp.setOverlay( o );
+		imp.updateAndDraw();
+		imp.show();
+		*/
 
 	}
 }
