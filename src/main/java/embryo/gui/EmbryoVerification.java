b9152603aa3bfd89de47b4e1ab2c150687088279
Stephan Preibisch <stephan.preibisch@gmx.de>
2019-03-25 12:07:15 +0100

    find and move ROI annotation

--- src/main/java/embryo/gui/EmbryoVerification.java
+++ src/main/java/embryo/gui/EmbryoVerification.java
@@ -2,6 +2,7 @@ package embryo.gui;
 
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -16,10 +17,13 @@ import embryo.gui.TextFileAccess.CSV_TYPE;
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
+import ij.gui.FreehandRoi;
 import ij.gui.GenericDialog;
 import ij.gui.ImageCanvas;
 import ij.gui.ImageWindow;
 import ij.gui.Overlay;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
 import ij.io.Opener;
 
 public class EmbryoVerification
@@ -95,6 +99,15 @@ public class EmbryoVerification
 			public void actionPerformed( ActionEvent e )
 			{
 				save();
+			}
+		} );
+
+		gui.addnew.addActionListener( new ActionListener()
+		{
+			@Override
+			public void actionPerformed( ActionEvent e )
+			{
+				addnew();
 			}
 		} );
 
@@ -236,6 +249,69 @@ public class EmbryoVerification
 			embryoList.get( i ).drawEllipse( dapiImpOverlay, i == embryoIndex );
 
 		dapiImp.setOverlay( dapiImpOverlay );
+		dapiImp.updateAndDraw();
+	}
+
+	public void addnew()
+	{
+		final Roi roi = dapiImp.getRoi();
+
+		if ( roi == null || !PolygonRoi.class.isInstance( roi ) )
+		{
+			IJ.log( "You need to draw a polygon or freehand roi." );
+			return;
+		}
+
+		// translate the ROI to the first quarter
+		final PolygonRoi fr = (PolygonRoi)roi;
+
+		final Rectangle bounds = fr.getBounds();
+
+		int x = (int)Math.round( bounds.getMinX() );
+		int y = (int)Math.round( bounds.getMinY() );
+		final int sx = (int)Math.round( bounds.getWidth() );
+		final int sy = (int)Math.round( bounds.getHeight() );
+
+		final boolean shiftX, shiftY;
+
+		if ( x + sx > dapiImp.getWidth() / 2 )
+			shiftX = true;
+		else
+			shiftX = false;
+
+		if ( y + sy > dapiImp.getHeight() / 2 )
+			shiftY = true;
+		else
+			shiftY = false;
+
+		if ( x < 0 || y < 0 )
+		{
+			IJ.log( "x: " + x );
+			IJ.log( "y: " + y );
+			IJ.log( "xn: " + (x - dapiImp.getWidth() / 2) );
+			IJ.log( "yn: " + (y - dapiImp.getHeight() / 2) );
+			IJ.log( "sx: " + sx);
+			IJ.log( "sy: " + sy );
+			IJ.log( "WARNING: You drew an invalid ROI, which is out of bounds after shifting it to the DAPI channel image (top left one)." );
+			return;
+		}
+
+		IJ.log( "ROI has " + fr.getNCoordinates() + " points " );
+
+		// bounding box of roi needs to be added to those coordinates
+		final int[] xpTmp = fr.getXCoordinates();
+		final int[] ypTmp = fr.getYCoordinates();
+
+		final int[] xp = new int[ fr.getNCoordinates() ];
+		final int[] yp = new int[ fr.getNCoordinates() ];
+
+		for ( int i = 0; i < xp.length; ++i )
+		{
+			xp[ i ] = Math.max( 0, xpTmp[ i ] + x - (shiftX ? dapiImp.getWidth() / 2 : 0 ) );
+			yp[ i ] = Math.max( 0, ypTmp[ i ] + y - (shiftY ? dapiImp.getHeight() / 2 : 0 ) );
+		}
+
+		dapiImp.setRoi( new PolygonRoi( xp, yp, xp.length, Roi.FREEROI ) );
 		dapiImp.updateAndDraw();
 	}
 
