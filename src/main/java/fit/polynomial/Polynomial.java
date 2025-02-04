583f6d388118c827d05a76241da841c6e8c2f146
Stephan Preibisch <stephan.preibisch@gmx.de>
2025-02-04 10:26:56 -0500

    update licenses

--- src/main/java/fit/polynomial/Polynomial.java
+++ src/main/java/fit/polynomial/Polynomial.java
@@ -2,7 +2,7 @@
  * #%L
  * code for function fitting
  * %%
- * Copyright (C) 2015 - 2023 Developers
+ * Copyright (C) 2015 - 2025 Developers
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
